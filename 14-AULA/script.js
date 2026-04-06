2cf8734a6e60c36321880bc1709593f11410ae88
Fabricio <mattiuzzifabricio@gmail.com>
2026-04-06 17:19:24 -0300

    começando js

---
+++ 14-AULA/script.js
@@ -0,0 +1,108 @@
+/*//MOSTRA UMA MENSAGEM NO CONSOLE
+console.log("Olá, mundo!");
+
+//MOSTRA UMA MENSAGEM DE ALERTA
+alert("Olá, mundo!");
+
+//CRIAMOS UMA VARIÁVEL
+var nome = "Fabricio"; //string ou cadeia
+
+var numero = 10;       //int ou inteiro
+
+var salto = 2.10       //float ou real
+
+var thebest = true;      //bool ou lógico
+console.log(typeof(nome));
+
+console.log("seu nome é " + nome);
+console.log("seu número é", numero);
+console.log("seu pulo é", salto);
+console.log("você é o melhor?", thebest);
+
+nome = 10
+console.log(typeof(nome));
+
+// outros tipos de variáveis
+var nulo = null
+var indefinido = undefined
+console.log(nulo)
+console.log(indefinido)
+*/
+//OPERADORES AERITMÉTICOS
+//+,-,*,/,%
+
+/*var a = 10 , b = 5
+
+console.log("Soma: ", a + b);
+console.log("Subtração: ", a - b);
+console.log("Multiplicação: ", a * b);
+console.log("Divisão: ", a / b);
+console.log("Módulo: ", a % b);
+
+//operadores logicos
+//E, OU, NÃO
+//&&, ||, !
+var vedade = true, mentira = false
+console.log(verdade && mentira);
+console.log(verdade || mentira);
+console.log(verdade && !mentira);
+
+//OPERADORES RELACIONAIS
+//>, <, >=, <=, ==, !=, ===, !==
+var x = 65, y = 32, z = "65"
+
+console.log(x > y);
+console.log(x < y);
+console.log(x >= y);
+console.log(x <= y);
+console.log(x == z);
+console.log(x != y);
+//verifica o valor e o tipo
+console.log(x === z);
+console.log(x !== z); */
+
+//desvios condicionais
+// se = if
+
+//var estaVivo = true
+
+/*/primeira comparação
+if(estaVivo == true){("parabéns, você está vivo!")
+}
+else if{
+    console.log("na verdade, você está vivo!")
+}
+else{
+    console.log("você está morto!")
+}
+*/
+
+/*/SWITCH = ESCOLHA
+var camisa = "vermelha"
+
+switch(camisa){
+    case "vermelha":
+        console.log("você ganhou um voucher");
+    break
+    case "azul":
+        console.log("você ganhou um desconto");
+    break
+    case "preta":
+        console.log("você ganhou um brinde");
+    break
+    default:
+        console.log("camisa inválida");
+    break
+} */
+//prompt - interação com o usuário
+/*prompt("Qual é o seu nome?")*/
+
+var c1 = prompt("Qual tamanho da caixa")
+var c2 = prompt("Qual tamanho da caixa")
+var c3 = prompt("Qual tamanho da caixa")
+
+var x = 65, y = 32, z = 188
+
+console.log(x < y < z);
+console.log("uma viagem")
+
